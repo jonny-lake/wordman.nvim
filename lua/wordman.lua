5fd42f9ceab05c3f243391984092f0832c8766c7
jonny-lake <jonathanjlake@gmail.com>
2023-02-23 08:33:18 -0700

    added key setup

--- lua/wordman.lua
+++ lua/wordman.lua
@@ -4,18 +4,47 @@ local M = {}
 
 -- setup
 function M.setup(obj)
-    Key = obj["key"]
-end
+    key = obj["key"]
 
-local host = 'wordsapiv1.p.rapidapi.com'
+    local host = 'wordsapiv1.p.rapidapi.com'
 
--- get synonyms
-local Synonyms = function(opts)
-    if Key ~= nil then
+    -- get synonyms
+    local Synonyms = function(opts)
+        if key ~= nil then
+            local word = string.lower(opts.args)
+            local req = http_request.new_from_uri(string.format('https://wordsapiv1.p.rapidapi.com/words/%s/synonyms', word))
+
+            req.headers:append('x-rapidapi-key', key)
+            req.headers:append('x-rapidapi-host', host)
+
+            local headers, stream = assert(req:go(10))
+
+            local body = assert(stream:get_body_as_string())
+
+            if headers:get ":status" ~= "200" then
+                error(body)
+            end
+
+            print(body)
+
+        else print("please set key with :WordmanSetKey")
+
+        end
+
+        end
+
+    vim.api.nvim_create_user_command(
+        'WordmanSynonyms',
+        Synonyms,
+        { nargs=1 }
+        )
+
+    -- get antonyms
+    local Antonyms = function(opts)
         local word = string.lower(opts.args)
-        local req = http_request.new_from_uri(string.format('https://wordsapiv1.p.rapidapi.com/words/%s/synonyms', word))
+        local req = http_request.new_from_uri(string.format('https://wordsapiv1.p.rapidapi.com/words/%s/antonyms', word))
 
-        req.headers:append('x-rapidapi-key', Key)
+        req.headers:append('x-rapidapi-key', key)
         req.headers:append('x-rapidapi-host', host)
 
         local headers, stream = assert(req:go(10))
@@ -28,133 +57,107 @@ local Synonyms = function(opts)
 
         print(body)
 
-    else print("please set key with :WordmanSetKey")
+    end
+
+    vim.api.nvim_create_user_command(
+        'WordmanAntonyms',
+        Antonyms,
+        { nargs=1 }
+        )
+
+    -- get definitions
+    local Definitions = function(opts)
+        local word = string.lower(opts.args)
+        local req = http_request.new_from_uri(string.format('https://wordsapiv1.p.rapidapi.com/words/%s/definitions', word))
+
+        req.headers:append('x-rapidapi-key', key)
+        req.headers:append('x-rapidapi-host', host)
+
+        local headers, stream = assert(req:go(10))
+
+        local body = assert(stream:get_body_as_string())
+
+        if headers:get ":status" ~= "200" then
+            error(body)
+        end
+
+        print(body)
 
     end
 
+    vim.api.nvim_create_user_command(
+        'WordmanDefinitions',
+        Definitions,
+        { nargs=1 }
+        )
+
+    -- get random
+    local Random = function()
+        local req = http_request.new_from_uri('https://wordsapiv1.p.rapidapi.com/words/?random=true')
+
+        req.headers:append('x-rapidapi-key', key)
+        req.headers:append('x-rapidapi-host', host)
+
+        local headers, stream = assert(req:go(10))
+
+        local body = assert(stream:get_body_as_string())
+
+        if headers:get ":status" ~= "200" then
+            error(body)
+        end
+
+        print(body)
+
     end
 
-vim.api.nvim_create_user_command(
-    'WordmanSynonyms',
-    Synonyms,
-    { nargs=1 }
-    )
+    vim.api.nvim_create_user_command(
+        'WordmanRandom',
+        Random,
+        { nargs=0 }
+        )
 
--- get antonyms
-local Antonyms = function(opts)
-    local word = string.lower(opts.args)
-    local req = http_request.new_from_uri(string.format('https://wordsapiv1.p.rapidapi.com/words/%s/antonyms', word))
+    -- get all
+    local All = function(opts)
 
-    req.headers:append('x-rapidapi-key', key)
-    req.headers:append('x-rapidapi-host', host)
+        Synonyms(opts)
+        Antonyms(opts)
+        Definitions(opts)
 
-    local headers, stream = assert(req:go(10))
-
-    local body = assert(stream:get_body_as_string())
-
-    if headers:get ":status" ~= "200" then
-        error(body)
     end
 
-    print(body)
 
-end
+    vim.api.nvim_create_user_command(
+        'WordmanAll',
+        All,
+        { nargs=1 }
+        )
 
-vim.api.nvim_create_user_command(
-    'WordmanAntonyms',
-    Antonyms,
-    { nargs=1 }
-    )
+    -- get word
+    local Word = function(opts)
+        local word = string.lower(opts.args)
+        local req = http_request.new_from_uri(string.format('https://wordsapiv1.p.rapidapi.com/words/%s', word))
 
--- get definitions
-local Definitions = function(opts)
-    local word = string.lower(opts.args)
-    local req = http_request.new_from_uri(string.format('https://wordsapiv1.p.rapidapi.com/words/%s/definitions', word))
+        req.headers:append('x-rapidapi-key', key)
+        req.headers:append('x-rapidapi-host', host)
 
-    req.headers:append('x-rapidapi-key', key)
-    req.headers:append('x-rapidapi-host', host)
+        local headers, stream = assert(req:go(10))
 
-    local headers, stream = assert(req:go(10))
+        local body = assert(stream:get_body_as_string())
 
-    local body = assert(stream:get_body_as_string())
+        if headers:get ":status" ~= "200" then
+            error(body)
+        end
 
-    if headers:get ":status" ~= "200" then
-        error(body)
+        print(body)
+
     end
 
-    print(body)
-
-end
-
-vim.api.nvim_create_user_command(
-    'WordmanDefinitions',
-    Definitions,
-    { nargs=1 }
-    )
-
--- get random
-local Random = function()
-    local req = http_request.new_from_uri('https://wordsapiv1.p.rapidapi.com/words/?random=true')
-
-    req.headers:append('x-rapidapi-key', key)
-    req.headers:append('x-rapidapi-host', host)
-
-    local headers, stream = assert(req:go(10))
-
-    local body = assert(stream:get_body_as_string())
-
-    if headers:get ":status" ~= "200" then
-        error(body)
+    vim.api.nvim_create_user_command(
+        'WordmanWord',
+        Word,
+        { nargs=1 }
+        )
     end
 
-    print(body)
 
-end
-
-vim.api.nvim_create_user_command(
-    'WordmanRandom',
-    Random,
-    { nargs=0 }
-    )
-
--- get all
-local All = function(opts)
-
-    Synonyms(opts)
-    Antonyms(opts)
-    Definitions(opts)
-
-end
-
-
-vim.api.nvim_create_user_command(
-    'WordmanAll',
-    All,
-    { nargs=1 }
-    )
-
--- get word
-local Word = function(opts)
-    local word = string.lower(opts.args)
-    local req = http_request.new_from_uri(string.format('https://wordsapiv1.p.rapidapi.com/words/%s', word))
-
-    req.headers:append('x-rapidapi-key', key)
-    req.headers:append('x-rapidapi-host', host)
-
-    local headers, stream = assert(req:go(10))
-
-    local body = assert(stream:get_body_as_string())
-
-    if headers:get ":status" ~= "200" then
-        error(body)
-    end
-
-    print(body)
-
-end
-
-vim.api.nvim_create_user_command(
-    'WordmanWord',
-    Word,
-    { nargs=1 }
-    )
+return M
